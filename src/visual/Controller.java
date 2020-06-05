4e2b1ea2cd152a05df92c5b06aa526de0961b69f
tatuan19 <trananhtuan12a10@gmail.com>
2020-06-05 16:18:04 +0700

    update controller

--- src/visual/Controller.java
+++ src/visual/Controller.java
@@ -130,13 +130,17 @@ public class Controller implements Initializable {
 			if (newValue.toString().compareTo("Linked List") == 0) {
 				insertButton.setText("Insert");
 				deleteButton.setText("Delete");
+				if(mainPane.getChildren() != null) {
 				mainPane.getChildren().remove(0, stack.getLength());
+				}
 				positionX = 10;
 				positionY = 150;
 			} else if (newValue.toString().compareTo("Stack") == 0) {
 				insertButton.setText("Push");
 				deleteButton.setText("Pop");
+				if(mainPane.getChildren() != null) {
 				mainPane.getChildren().remove(0, linkedList.getLength());
+				}
 				positionX = 150;
 				positionY = 150;
 			}
