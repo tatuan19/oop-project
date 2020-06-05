ff08a4093f326da2d18ba37972828ca9152748e4
tatuan19 <trananhtuan12a10@gmail.com>
2020-06-05 17:19:52 +0700

    update dimension

--- src/element/Dimensions.java
+++ src/element/Dimensions.java
@@ -3,10 +3,10 @@ package element;
 public class Dimensions {
     public static final int WIDTH =30;
     public static final int HEIGHT =30;
-    public static final int SCENE_WIDTH = 555;
+    public static final int SCENE_WIDTH = 830;
     public static final int SCENE_HEIGHT =536;
 
-    public static final int PANE_WIDTH = 332;
+    public static final int PANE_WIDTH = 600;
     public static final int PANE_HEIGHT = 332;
     
     public static Element.Type ELEMENT_TYPE = Element.Type.CIRCLE;
